d4749713303ef6be1eda480216a8246f70237e29
andrewkuhl <andrewkuhlz@gmail.com>
2022-03-21 12:01:54 -0700

    restting

--- js/filter.js
+++ js/filter.js
@@ -1,16 +1,3 @@
-var counterContainer = document.querySelector(".website-counter");
-var resetButton = document.querySelector("#reset");
-var visitCount = localStorage.getItem("page_view");
-
-// Check if page_view entry is present
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  localStorage.setItem("page_view", visitCount);
-} else {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-}
-
 const filterContainer = document.querySelector(".gal-buttons");
 const galleryItems = document.querySelectorAll(".gal-obj");
 
