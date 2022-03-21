02c76b9b61e9d9ab76abfa26e9981e4d043e7d59
Andrew Kuhl <andrewkuhlz@gmail.com>
2022-03-21 11:49:07 -0700

    filtered gallery

--- script.js
+++ script.js
@@ -10,4 +10,33 @@ if (visitCount) {
   visitCount = 1;
   localStorage.setItem("page_view", 1);
 }
-counterContainer.innerHTML = visitCount;
+
+const filterContainer = document.querySelector(".gal-buttons");
+const galleryItems = document.querySelectorAll(".gal-obj");
+
+filterContainer.addEventListener("click", (event) =>{
+  if(event.target.classList.contains("gal-button")){
+
+    // deactivate existing active 'filter-item'
+    filterContainer.querySelector(".active").classList.remove("active");
+
+    // activate new 'filter-item'
+    event.target.classList.add("active");
+
+    const filterValue = event.target.getAttribute("data-filter");
+
+    galleryItems.forEach((item) =>{
+
+      if(item.classList.contains(filterValue) || filterValue === 'all'){
+        item.classList.remove("hide");
+         item.classList.add("show");
+      }
+
+      else{
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+
+     });
+  }
+});
